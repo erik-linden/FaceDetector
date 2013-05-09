erik-linden/FaceDetector

commit 055f1c4a459d1ab7d64b661171ca84f051193abc

Better parametrization of ImageScanner

ImageScanner.java
@@ -18,8 +18,10 @@ public class ImageScanner {
 
 	HaarFeature f;
 	CascadeClassifier classifier;
-	double pixelSkip = 1.1;
-	double scaleSkip = 1.15;
+	double pixelSkip = 1.0;
+	double scaleSkip = 1.10;
+	double thld_gain = 1.3;
+	double startScale = 3;
 
 	public static void main(String[] args) {
 		File folder = new File(FileUtils.combinePath(EnvironmentConstants.PROJECT_ROOT, "TestImages"));
@@ -58,7 +60,7 @@ public class ImageScanner {
 	Vector<Detection> scan() {
 		Vector<Detection> list = new Vector<>();
 		int nTests = 0;
-		double scale = 2.0;
+		double scale = startScale;
 
 		int w = (int) Math.round(((double)HaarFeature.MIN_PATCH_SIDE)*scale);
 		while (w<=Math.min(f.img.height, f.img.width)) {
@@ -103,7 +105,7 @@ public class ImageScanner {
 			}
 			
 			double thld_adj = classifier.cascadeThlds.get(l);
-			if(sumH<sumA/2*thld_adj*1.35) {
+			if(sumH<sumA/2*thld_adj*thld_gain) {
 				return false;
 			}
 

commit ede33d4172a286afb985eaf6ca632ace95a24f22

Make plotting of result work for me
Commit 055f1c4a459d1ab7d64b661171ca84f051193abc broke it somehow.

ImageScanner.java
@@ -41,14 +41,6 @@ public class ImageScanner {
 		
 		imgScanner.f = new HaarFeature(IntegralImage.makeIntegralImage(file));
 
-		JFrame frame = new JFrame();
-		ImageIcon icon = new ImageIcon();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout());
-		frame.getContentPane().add(new JLabel(icon));
-		frame.pack();
-		frame.setVisible(true);
-		
 		long startTime = System.currentTimeMillis();
 		List<Detection> list = imgScanner.scan();
 		System.out.println((System.currentTimeMillis()-startTime));
@@ -57,7 +49,14 @@ public class ImageScanner {
 		//				System.out.println("x: "+c.x+" y: "+c.y+" w: "+c.w);
 		//			}
 		System.out.println("Found: "+list.size());
-		drawBoundingBoxes(ImageIO.read(file), list, 1, icon);
+
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(new JLabel(drawBoundingBoxes(ImageIO.read(file), list, 1)));
+		frame.pack();
+		frame.setVisible(true);
+		
 	}
 
 	List<Detection> scan() {
@@ -117,7 +116,7 @@ public class ImageScanner {
 		return true;
 	}
 
-	static void drawBoundingBoxes(BufferedImage srcImage, List<Detection> list, double scaleFactor, ImageIcon icon) {
+	static ImageIcon drawBoundingBoxes(BufferedImage srcImage, List<Detection> list, double scaleFactor) {
 
 		int width = srcImage.getWidth();
 		int height = srcImage.getHeight();
@@ -142,8 +141,8 @@ public class ImageScanner {
 			}
 		}
 		
-		icon.setImage(img.getScaledInstance(
-								(int)(width*scaleFactor), (int)(height*scaleFactor), Image.SCALE_SMOOTH));
+		return new ImageIcon(img.getScaledInstance((int) (width * scaleFactor),
+				(int) (height * scaleFactor), Image.SCALE_SMOOTH));
 
 	}
 }