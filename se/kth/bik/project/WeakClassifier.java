619cd8bc77fe29cef386771dd8204698008bc501
Emil Lundberg <emlun@kth.se>
2013-05-12 18:52:45 +0200

    Add some javadoc to WeakClassifier

--- se/kth/bik/project/WeakClassifier.java
+++ se/kth/bik/project/WeakClassifier.java
@@ -21,10 +21,27 @@ public class WeakClassifier implements java.io.Serializable {
 	    return parity * featureValue > parity * thld;
 	}
 
+    /**
+     * @param fv
+     *            a vector of feature values corresponding to different
+     *            features. From this vector, <code>fv[index]</code> will be
+     *            used, where <code>index</code> is the value of this
+     *            WeakClassifier's {@link #index} member.
+     * @return <code>true</code> if the given feature value (the value at index
+     *         <code>index</code> in <code>fv</code>) is classified as a face by
+     *         this classifier.
+     */
 	public boolean classify(double[] fv) {
 	    return classify(fv[index]);
 	}
 
+    /**
+     * @param feature
+     *            the {@link HaarFeature} instance which will compute the
+     *            feature value.
+     * @return <code>true</code> if the feature value computed by
+     *         <code>feature</code> is classified as a face by this classifier.
+     */
 	public boolean classify(HaarFeature feature) {
 	    return classify(feature.computeFeature(index));
 	}
